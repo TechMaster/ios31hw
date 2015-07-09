0bfd5a256b5c0807b866d58ffde672e33faf52b1
Cuong Trinh <cuong@cuong.local>
2015-07-09 19:20:53 +0700

    So cool

---
+++ DemoGIT/DemoGIT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoGIT
+//
+//  Created by Cuong Trinh on 7/9/15.
+//  Copyright (c) 2015 Cuong Trinh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DemoGIT/DemoGIT/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DemoGIT
+//
+//  Created by Cuong Trinh on 7/9/15.
+//  Copyright (c) 2015 Cuong Trinh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
